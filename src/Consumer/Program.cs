adbf9028872e89b6a78dd5ddce3be24d8f760212
Abdurrahman Işık <isikabdurrahman@yahoo.com>
2022-02-20 19:12:13 +0200

    minor improvements

--- src/Consumer/Program.cs
+++ src/Consumer/Program.cs
@@ -4,76 +4,45 @@ using RabbitMQ.Client.Events;
 
 Console.WriteLine("__Consumer__");
 
-// Topic Exchange
-// var factory = new ConnectionFactory() { HostName = "localhost" };
-// using(var connection = factory.CreateConnection())
-// using(var channel = connection.CreateModel())
-// {
-//     channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
-//
-//     var queueName = channel.QueueDeclare().QueueName;
-//     channel.QueueBind(queue: queueName,
-//         exchange: "logs",
-//         routingKey: "");
-//
-//     Console.WriteLine(" [*] Waiting for logs.");
-//
-//     var consumer = new EventingBasicConsumer(channel);
-//     consumer.Received += (model, ea) =>
-//     {
-//         var body = ea.Body.ToArray();
-//         var message = Encoding.UTF8.GetString(body);
-//         Console.WriteLine(" [x] {0}", message);
-//     };
-//     channel.BasicConsume(queue: queueName,
-//         autoAck: true,
-//         consumer: consumer);
-//
-//     Console.WriteLine(" Press [enter] to exit.");
-//     Console.ReadLine();
-// }
-
 var factory = new ConnectionFactory() { HostName = "localhost" };
-using(var connection = factory.CreateConnection())
-using(var channel = connection.CreateModel())
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.ExchangeDeclare(exchange: "direct_logs",
+    type: "direct");
+var queueName = channel.QueueDeclare().QueueName;
+
+if(args.Length < 1)
 {
-    channel.ExchangeDeclare(exchange: "direct_logs",
-        type: "direct");
-    var queueName = channel.QueueDeclare().QueueName;
-
-    if(args.Length < 1)
-    {
-        Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
-            Environment.GetCommandLineArgs()[0]);
-        Console.WriteLine(" Press [enter] to exit.");
-        Console.ReadLine();
-        Environment.ExitCode = 1;
-        return;
-    }
-
-    foreach(var severity in args)
-    {
-        Console.WriteLine("Severity:{0}", severity);
-        channel.QueueBind(queue: queueName,
-            exchange: "direct_logs",
-            routingKey: severity);
-    }
-
-    Console.WriteLine(" [*] Waiting for messages.");
-
-    var consumer = new EventingBasicConsumer(channel);
-    consumer.Received += (model, ea) =>
-    {
-        var body = ea.Body.ToArray();
-        var message = Encoding.UTF8.GetString(body);
-        var routingKey = ea.RoutingKey;
-        Console.WriteLine(" [x] Received '{0}':'{1}'",
-            routingKey, message);
-    };
-    channel.BasicConsume(queue: queueName,
-        autoAck: true,
-        consumer: consumer);
-
+    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+        Environment.GetCommandLineArgs()[0]);
     Console.WriteLine(" Press [enter] to exit.");
     Console.ReadLine();
+    Environment.ExitCode = 1;
+    return;
 }
+
+foreach(var severity in args)
+{
+    Console.WriteLine("Severity:{0}", severity);
+    channel.QueueBind(queue: queueName,
+        exchange: "direct_logs",
+        routingKey: severity);
+}
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var routingKey = ea.RoutingKey;
+    Console.WriteLine(" [x] Received '{0}':'{1}'",
+        routingKey, message);
+};
+channel.BasicConsume(queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
