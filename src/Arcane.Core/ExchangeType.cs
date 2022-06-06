2b01145f19ecfce2a376b7edba3ddccf9b428a6f
Abdurrahman Işık <isikabdurrahman@yahoo.com>
2022-06-06 23:59:12 +0300

    add core project

---
+++ src/Arcane.Core/ExchangeType.cs
@@ -0,0 +1,8 @@
+﻿namespace Arcane.Core;
+
+public static class ExchangeType
+{
+    public const string Direct = "direct";
+    public const string Fanout = "fanout";
+    public const string Topic = "topic";
+}
