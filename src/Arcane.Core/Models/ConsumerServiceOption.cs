ee7b4ed29cd87c7a020b45db305dc08dcc339ce4
Abdurrahman Işık <isikabdurrahman@yahoo.com>
2022-06-08 21:35:54 +0300

    add constructor method

--- src/Arcane.Core/Models/ConsumerServiceOption.cs
+++ src/Arcane.Core/Models/ConsumerServiceOption.cs
@@ -4,6 +4,18 @@ public class ConsumerServiceOption
 {
     public IServiceProvider ServiceProvider { get; set; }
     public object Option { get; set; }
-    public Type bodyType { get; set; }
+    public Type BodyType { get; set; }
     public Func<object, DeliverProperties, Task> ConsumerAction { get; set; }
+
+    public ConsumerServiceOption(
+        IServiceProvider serviceProvider,
+        object option,
+        Type bodyType,
+        Func<object, DeliverProperties, Task> consumerAction)
+    {
+        ServiceProvider = serviceProvider;
+        Option = option;
+        BodyType = bodyType;
+        ConsumerAction = consumerAction;
+    }
 }
